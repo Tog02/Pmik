c38dcd236477719042a61f43add6951a666ef030
Tomasz Głowacki <tomaszg687@gmail.com>
2025-04-01 15:39:06 +0200

    Stworzenie repo

---
+++ Monitorek/Monitorek.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "ssd1306_i2c.c"
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+#define TOTAL_RECORDS 6
+uint8_t buf[SSD1306_BUF_LEN];
+char *records[TOTAL_RECORDS] = {
+    "SIN 1000Hz",
+    "SIN 2000Hz",
+    "SIN 5000Hz",
+    "SIN 10000Hz",
+    "",
+    ""};
+
+int selectedRecord = 0; // Indeks aktualnie wybranego nagrania
+
+struct render_area frame_area = {
+    start_col : 0,
+    end_col : SSD1306_WIDTH - 1,
+    start_page : 0,
+    end_page : SSD1306_NUM_PAGES - 1
+};
+
+void DisplayRecords(void)
+{
+    SSD1306_send_cmd(SSD1306_SET_ALL_ON);
+    sleep_ms(500);
+
+    SSD1306_send_cmd(SSD1306_SET_ENTIRE_ON); // Set all pixels on
+    sleep_ms(500);
+
+    for (int i = 0; i < TOTAL_RECORDS; i++)
+    {
+        // Dodaj strzałkę przed wybranym nagraniem
+        if (i == selectedRecord)
+        {
+            // ssd1306_SetCursor(0, i * 10); // Pozycja w pionie zależy od wiersza
+            WriteChar(buf, 5, i * 8, '1');
+        }
+
+        // Wyświetl nazwę nagrania
+        // ssd1306_SetCursor(10, i * 10);
+        WriteString(buf, 10, i * 8, records[i]);
+    }
+
+    render(buf, &frame_area);
+    sleep_ms(2000);
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // I2C Initialisation. Using it at 400Khz.
+    const uint LED_PIN = 19; // Wbudowana dioda LED na Raspberry Pi Pico
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    i2c_init(I2C_PORT, 400 * 1000);
+    SSD1306_init();
+    calc_render_area_buflen(&frame_area);
+    // maid();
+    //  For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    while (true)
+    {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+        DisplayRecords();
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(LED_PIN, 0);
+    }
+}
---
+++ sd_card_v1/sd_card_v1.c
@@ -0,0 +1,178 @@
+/* main.c
+Copyright 2021 Carl John Kugler III
+
+Licensed under the Apache License, Version 2.0 (the License); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+
+#include <stdio.h>
+//
+#include "stdlib.h"
+//
+#include "hw_config.h"
+#include "f_util.h"
+#include "ff.h"
+
+/**
+ * @file main.c
+ * @brief Minimal example of writing to a file on SD card
+ * @details
+ * This program demonstrates the following:
+ * - Initialization of the stdio
+ * - Mounting and unmounting the SD card
+ * - Opening a file and writing to it
+ * - Closing a file and unmounting the SD card
+ */
+void polinii();
+void wwr()
+{
+    FATFS fs;
+    FRESULT fr = f_mount(&fs, "./", 1);
+    if (FR_OK != fr)
+    {
+        panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+
+    // Open a file and write to it
+    FIL fil;
+    const char *const filename = "filename.txt";
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if (FR_OK != fr && FR_EXIST != fr)
+    {
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    }
+    if (f_printf(&fil, "Hello, world!\n") < 0)
+    {
+        printf("f_printf failed\n");
+    }
+
+    if (fr == FR_OK)
+    {
+        gpio_put(16, 1);
+    }
+    else
+    {
+        gpio_put(16, 0);
+    }
+
+    // Close the file
+    fr = f_close(&fil);
+    if (FR_OK != fr)
+    {
+        printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+    if (fr == FR_OK)
+    {
+        gpio_put(16, 1);
+    }
+    else
+    {
+        gpio_put(16, 0);
+    }
+    // Unmount the SD card
+    f_unmount("");
+}
+
+int main()
+{
+    // Initialize stdio
+    stdio_init_all();
+    gpio_init(16);              // Inicjalizacja pinu GPIO 16
+    gpio_set_dir(16, GPIO_OUT); // Ustawienie jako wyjście
+    printf("Hello, world!");
+
+    // See FatFs - Generic FAT Filesystem Module, "Application Interface",
+    // http://elm-chan.org/fsw/ff/00index_e.html
+    FATFS fs;
+    FRESULT fr = f_mount(&fs, "./", 1);
+    if (FR_OK != fr)
+    {
+        panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+
+    // Open a file and write to it
+    FIL fil;
+    const char *const filename = "filename.txt";
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if (FR_OK != fr && FR_EXIST != fr)
+    {
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    }
+    if (f_printf(&fil, "Hello, world!\n") < 0)
+    {
+        printf("f_printf failed\n");
+    }
+
+    // Close the file
+    fr = f_close(&fil);
+    if (FR_OK != fr)
+    {
+        printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+
+    UINT bw, br;
+    char readBuffer[50];
+
+    if (f_open(&fil, "www.txt.txt", FA_READ) == FR_OK)
+    {
+        f_read(&fil, readBuffer, sizeof(readBuffer) - 1, &br);
+        readBuffer[br] = '\0'; // Dodanie końca stringa
+
+        f_close(&fil);
+    }
+    if (f_open(&fil, "www.txt.txt", FA_OPEN_APPEND | FA_WRITE) == FR_OK)
+    {
+        f_write(&fil, readBuffer, br + 1, &bw);
+        f_close(&fil);
+    }
+    // Unmount the SD card
+    f_unmount("");
+
+    printf("Goodbye, world!");
+    while (1)
+    {
+        wwr();
+        printf("witam\n");
+        printf("Odczytane dane: %s\n", readBuffer);
+        sleep_ms(1000);
+        gpio_put(16, 0);
+        sleep_ms(300);
+        polinii();
+    }
+}
+
+void polinii()
+{
+    FATFS fs;
+    FIL file;
+    char line[100]; // Bufor na linię
+    // Montowanie systemu plików
+    f_mount(&fs, "", 0);
+
+    // Otwieranie pliku do odczytu
+    if (f_open(&file, "test.txt", FA_READ) == FR_OK)
+    {
+        DWORD fileSize = file.obj.objsize;
+        printf("Rozmiar pliku: %lu bajtów\n", fileSize);
+        // Czytanie linii po linii
+        while (f_gets(line, sizeof(line), &file))
+        {
+            printf("Odczytana linia: %s", line); // Drukujemy linię
+        }
+        f_close(&file);
+    }
+    else
+    {
+        printf("Nie można otworzyć pliku!\n");
+    }
+
+    // Odmontowanie systemu plików
+    f_mount(NULL, "", 0);
+}
